4d33e7438ab46de86379a4c1660b72e299176930
jufentosemrylake <jufentosemry@gmail.com>
2022-07-09 22:25:17 +0700

    edit app config 2

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://herregistrasi-unwira.herokuapp.com/'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
